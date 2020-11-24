11b0bd4570928e79bf64431494d82aaee9a17a77
moe <moe.faridd@gmail.com>
2020-11-23 19:09:55 -0500

    python warmup

---
+++ testing.py
@@ -0,0 +1,19 @@
+print('hello man')
+#result hello man
+print('hello' + 'world')
+#result helloworld
+print('hello ' + "1")
+#hello 1
+print('hello ' + str(2)) #str typecast to str
+#hello 2
+print(f'Look at this... {5-9}')
+#Look at this...    -4 note the f
+one = 'Hello'
+two = "Worldly"
+print(one + ' ' + two) #concatenation
+#Hello Worldly
+print(f'{one} {two}') #interpolation
+#Hello Worldly
+
+
+
